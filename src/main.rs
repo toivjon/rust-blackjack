2e603d5659c48ef87db34b0b2960f96a1395ff6d
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2022-10-16 20:29:48 +0300

    Added a small main menu for the game.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::io::stdin;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        println!("================");
+        println!("BLACKJACK - MENU");
+        println!("1 : play game");
+        println!("2 : quit game");
+        println!("Enter selection:");
+
+        let mut input = String::new();
+        stdin().read_line(&mut input).expect("Invalid input");
+
+        match input.trim() {
+            "1" => println!("TODO Play Game!"),
+            "2" => break,
+            _ => continue,
+        }
+    }
 }
