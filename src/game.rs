4c1bb964df795a765eae5545bee67462ffcdf777
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2022-10-20 08:03:12 +0300

    Added a placeholder about how to resolve card points.

--- src/game.rs
+++ src/game.rs
@@ -57,17 +57,30 @@ pub fn play() {
 
     let mut players_hand: Vec<Card> = Vec::new();
     players_hand.push(deck.remove(1)); // TODO select with rng
-    players_hand.push(deck.remove(1)); // TODO select with rng
+    players_hand.push(deck.remove(8)); // TODO select with rng
 
     loop {
+        let (dealer_points, dealer_alt_points) = count_points(&dealers_hand);
+        let (player_points, player_alt_points) = count_points(&players_hand);
+
         println!("=================");
         println!("BLACKJACK - ROUND");
         println!("Dealers hand:");
-        println!("{}", dealers_hand[0]);
-        println!("{}", dealers_hand[1]); // TODO temporarily show this during dev.
+        println!("  {}", dealers_hand[0]);
+        println!("  {}", dealers_hand[1]); // TODO temporarily show this during dev.
+        if dealer_points != dealer_alt_points {
+            println!("  total {}/{}", dealer_points, dealer_alt_points)
+        } else {
+            println!("  total {}", dealer_points)
+        }
         println!("Players hand:");
-        println!("{}", players_hand[0]);
-        println!("{}", players_hand[1]);
+        println!("  {}", players_hand[0]);
+        println!("  {}", players_hand[1]);
+        if player_points != player_alt_points {
+            println!("  total {}/{}", player_points, player_alt_points)
+        } else {
+            println!("  total {}", player_points)
+        }
         println!("1 : Hit");
         println!("2 : Stand");
         println!("Enter selection:");
@@ -94,6 +107,30 @@ fn build_deck() -> Vec<Card> {
         }
     }
     deck
+}
+
+fn count_points(cards: &Vec<Card>) -> (usize, usize) {
+    get_points_for_cards(cards)
+}
+
+fn get_points_for_cards(cards: &Vec<Card>) -> (usize, usize) {
+    let mut points = (0, 0);
+    for card in cards {
+        let card_points = get_points_for_card(&card);
+        points.0 += card_points.0;
+        points.1 += card_points.1;
+    }
+    points
+}
+
+// TODO move as a Card member function?
+fn get_points_for_card(card: &Card) -> (usize, usize) {
+    match card.rank {
+        1 => (1, 14),
+        2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 => (card.rank, card.rank),
+        11 | 12 | 13 => (10, 10),
+        e => panic!("Unsupported rank: {e}"),
+    }
 }
 
 fn wait_selection() -> Decision {
@@ -126,6 +163,26 @@ mod tests {
                 assert!(ranks[i].suit == suit);
                 assert!(ranks[i].rank == i + 1);
             }
+        }
+    }
+
+    #[test]
+    fn count_points_with_empty_slice() {
+        let deck: Vec<Card> = vec![];
+        assert_eq!((0, 0), count_points(&deck));
+    }
+
+    #[test]
+    fn count_points_with_one_card() {
+        let deck = build_deck();
+        for card in deck {
+            let points = match card.rank {
+                1 => (1, 14),
+                2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 => (card.rank, card.rank),
+                11 | 12 | 13 => (10, 10),
+                e => panic!("Unsupported rank: {e}"),
+            };
+            assert_eq!(points, count_points(&vec![card]));
         }
     }
 }
