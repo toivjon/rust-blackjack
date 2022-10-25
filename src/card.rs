ad62d0f909116ad0a5426b6c070ea649861c8cab
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2022-10-25 07:58:33 +0300

    Added the missing unit tests for the Card struct.

--- src/card.rs
+++ src/card.rs
@@ -1,15 +1,31 @@
 use std::fmt;
 
-use crate::{rank::Rank, suit::Suit};
+use crate::rank;
+use crate::suit;
 
 #[derive(PartialEq, Eq)]
 pub struct Card {
-    pub suit: Suit,
-    pub rank: Rank,
+    pub suit: suit::Suit,
+    pub rank: rank::Rank,
 }
 
 impl fmt::Display for Card {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}{}", self.rank, self.suit)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_display() {
+        for suit in suit::values() {
+            for rank in rank::values() {
+                let val = format!("{}{}", rank, suit);
+                assert_eq!(val, Card { suit, rank }.to_string());
+            }
+        }
     }
 }
